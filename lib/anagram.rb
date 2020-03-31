7708441a0a0d68dddae4cc7f94cebdbab37a70f1
Sana Zaidi <sana.rizvi.5@gmail.com>
2020-03-31 04:21:37 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,15 +4,14 @@ class Anagram
   
   attr_reader :name
   
-def initialize(name)
-@name = name 
-end 
+  def initialize(name)
+  @name = name 
+  end 
 
-def match(array) 
-  array.keep_if do { |string| } 
-  name.split('').sort == string.split('').sort
-end
-  
+  def match(array) 
+    array.keep_if do { |string| } 
+    name.split('').sort == string.split('').sort
+  end
 end 
   
 end 
