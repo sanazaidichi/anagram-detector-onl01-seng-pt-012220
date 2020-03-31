3267440c3d763219b50fba223fa4a5ff78f9fa4a
Sana Zaidi <sana.rizvi.5@gmail.com>
2020-03-31 04:06:17 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram 
+  
+  attr_reader :name
+  
+def initialize(name)
+@name = name 
+end 
+
+def match(array) 
+  array.keep_if { |string| binding.pry } 
+  
+end 
+  
+end 
