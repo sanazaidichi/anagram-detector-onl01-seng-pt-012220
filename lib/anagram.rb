ba212a92e651fd8768e99785e5d8aeac8ad5d032
sanazaidichi <sana.rizvi.5@gmail.com>
2020-06-15 14:35:00 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,14 +1,35 @@
 require 'pry'
 
-class Anagram 
-attr_reader :name
-  
-  def initialize(name)
-   @name = name 
-  end 
 
-  def match(arr) 
-    arr.keep_if { |str| name.split('').sort == str.split('').sort }
-  end
+class Anagram 
+
 end 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#class Anagram 
+#attr_reader :name
+  
+#  def initialize(name)
+#   @name = name 
+#  end 
+
+#  def match(arr) 
+#    arr.keep_if { |str| name.split('').sort == str.split('').sort }
+#  end
+#end 
   
